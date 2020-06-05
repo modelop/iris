016ae2b790c099d16583e4511099b785f9857e61
benmackenzie-exos <ben.mackenzie@exosfinancial.com>
2020-06-05 11:59:49 -0400

    add model

---
+++ iris.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import argparse
+import pandas as pd
+
+from sklearn import tree
+import pickle
+
+
+def begin():
+
+    global model
+    model = pickle.load(open('model.pkl', 'rb'))
+
+
+
+def train(train_data):
+
+
+    # labels are in the first column
+    train_y = train_data.iloc[:, 0]
+    train_X = train_data.iloc[:, 1:]
+
+    max_leaf_nodes = 30
+
+    # Now use scikit-learn's decision tree classifier to train the model.
+    clf = tree.DecisionTreeClassifier(max_leaf_nodes=max_leaf_nodes)
+    clf = clf.fit(train_X, train_y)
+
+    # Print the coefficients of the trained classifier, and save the coefficients
+
+    pickle.dump(clf, open('model.pkl', 'wb'))
+
+
+
+
+def metrics(df):
+    y_test = df.iloc[:, 0]
+    X_test = df.iloc[:, 1:]
+    yield { "ACCURACY": model.score(X_test, y_test)}
+
+
+def action(df):
+    X_test = df.iloc[:, 1:]
+    y_pred = model.predict(X_test)
+    yield y_pred
+
+
+
+if __name__ == "__main__":
+    train_df = pd.read_csv('data/iris.csv')
+
+
+    train(train_df)
+    begin()
+
+
+    for m in metrics(train_df):
+        print(m)
+
+    for a in action(train_df):
+        print(a)
